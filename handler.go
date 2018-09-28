439e818597d0d18d6ab871fc1e2b8d074413f9ad
Roman Nekhoroshev <roman@motiveinteractive.com>
2018-09-28 14:47:14 -0700

    * Make rate limiter easier to extend * Add `SimpleLimitHandler` limiter that covers the most common scenario (just send back a status code)

--- handler.go
+++ handler.go
@@ -6,16 +6,23 @@ import (
 )
 
 // LimitHandler is a very simple FastHTTP adapter for `github.com/bsm/ratelimit`. It wraps original request handler with a rate-limited one
-func LimitHandler(handler fasthttp.RequestHandler, limiter *ratelimit.RateLimiter, statusCode int) fasthttp.RequestHandler {
+func LimitHandler(handler fasthttp.RequestHandler, limiter *ratelimit.RateLimiter, onLimitReachedHandler fasthttp.RequestHandler) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
 		shouldLimit := limiter.Limit()
 
 		if shouldLimit {
-			ctx.SetStatusCode(statusCode)
-			ctx.SetBody([]byte("You have reached maximum request limit."))
+			onLimitReachedHandler(ctx)
 			return
 		}
 
 		handler(ctx)
 	}
+}
+
+// SimpleLimitHandler covers the most simple rate limiting case when the server only needs to return a predefined status code.
+func SimpleLimitHandler(handler fasthttp.RequestHandler, limiter *ratelimit.RateLimiter, statusCode int) fasthttp.RequestHandler {
+	return LimitHandler(handler, limiter, func(ctx *fasthttp.RequestCtx) {
+		ctx.SetStatusCode(statusCode)
+		ctx.SetBody([]byte("You have reached maximum request limit."))
+	})
 }
